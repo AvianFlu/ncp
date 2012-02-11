edd0cdc393501da72647950dc234e360c3aba8f6
Vyacheslav Bazhinov <SlNPacifist@gmail.com>
2012-02-11 17:54:52 +0700

    Revert: Added tests and small fix for situations when callback was not called if several files or dirs were filtered out (86114b84632b9e05b93c40fcc0df4155d46f456a)

--- test/ncp-test.js
+++ test/ncp-test.js
@@ -35,44 +35,5 @@ vows.describe('ncp').addBatch({
       }
     }
   }
-}).addBatch({
-  'When using ncp': {
-    'and copying files using filter': {
-      topic: function() {
-        var cb = this.callback;
-        var filter = function(name) {
-          return name.substr(name.length - 1) != 'a'
-        }
-        rimraf(out, function () {
-          ncp(src, out, {filter: filter}, cb);
-        });
-      },
-      'it should copy files': {
-        topic: function () {
-          var cb = this.callback;
-
-          readDirFiles(src, 'utf8', function (srcErr, srcFiles) {
-            function filter(files) {
-              for (var fileName in files) {
-                var curFile = files[fileName];
-                if (curFile instanceof Object)
-                  return filter(curFile);
-                if (fileName == 'a')
-                  delete files[fileName];
-              }
-            }
-            filter(srcFiles);
-            readDirFiles(out, 'utf8', function (outErr, outFiles) {
-              cb(outErr, srcFiles, outFiles);
-            });
-          });
-        },
-        'and destination files should match source files that pass filter': function (err, srcFiles, outFiles) {
-          assert.isNull(err);
-          assert.deepEqual(srcFiles, outFiles);
-        }
-      }
-    }
-  }
 }).export(module);
 
