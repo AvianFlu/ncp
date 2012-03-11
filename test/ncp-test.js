ca24fdaa658a7a27fc56bb25a75908e503148a76
Charlie McConnell <charlie@charlieistheman.com>
2012-03-11 13:05:35 -0700

    [test fix] Clean up tests a little.

--- test/ncp-test.js
+++ test/ncp-test.js
@@ -10,67 +10,63 @@ var fixtures = path.join(__dirname, 'fixtures'),
     out = path.join(fixtures, 'out');
 
 vows.describe('ncp').addBatch({
-  'When using `ncp`': {
-    'and copying files from one directory to another': {
+  'When copying a directory of files': {
+    topic: function () {
+      var cb = this.callback;
+      rimraf(out, function () {
+        ncp(src, out, cb);
+      });
+    },
+    'files should be copied': {
       topic: function () {
         var cb = this.callback;
-        rimraf(out, function () {
-          ncp(src, out, cb);
+
+        readDirFiles(src, 'utf8', function (srcErr, srcFiles) {
+          readDirFiles(out, 'utf8', function (outErr, outFiles) {
+            cb(outErr, srcFiles, outFiles);
+          });
         });
       },
-      'it should copy files': {
-        topic: function () {
-          var cb = this.callback;
-
-          readDirFiles(src, 'utf8', function (srcErr, srcFiles) {
-            readDirFiles(out, 'utf8', function (outErr, outFiles) {
-              cb(outErr, srcFiles, outFiles);
-            });
-          });
-        },
-        'and destination files should match source files': function (err, srcFiles, outFiles) {
-          assert.isNull(err);
-          assert.deepEqual(srcFiles, outFiles);
-        }
+      'and the destination should match the source': function (err, srcFiles, outFiles) {
+        assert.isNull(err);
+        assert.deepEqual(srcFiles, outFiles);
       }
     }
   }
 }).addBatch({
-  'When using ncp': {
-    'and copying files using filter': {
-      topic: function() {
+  'When copying files using filter': {
+    topic: function() {
+      var cb = this.callback;
+      var filter = function(name) {
+        return name.substr(name.length - 1) != 'a'
+      }
+      rimraf(out, function () {
+        ncp(src, out, {filter: filter}, cb);
+      });
+    },
+    'it should copy files': {
+      topic: function () {
         var cb = this.callback;
-        var filter = function(name) {
-          return name.substr(name.length - 1) != 'a'
-        }
-        rimraf(out, function () {
-          ncp(src, out, {filter: filter}, cb);
+
+        readDirFiles(src, 'utf8', function (srcErr, srcFiles) {
+          function filter(files) {
+            for (var fileName in files) {
+              var curFile = files[fileName];
+              if (curFile instanceof Object)
+                return filter(curFile);
+              if (fileName.substr(fileName.length - 1) == 'a')
+                delete files[fileName];
+            }
+          }
+          filter(srcFiles);
+          readDirFiles(out, 'utf8', function (outErr, outFiles) {
+            cb(outErr, srcFiles, outFiles);
+          });
         });
       },
-      'it should copy files': {
-        topic: function () {
-          var cb = this.callback;
-
-          readDirFiles(src, 'utf8', function (srcErr, srcFiles) {
-            function filter(files) {
-              for (var fileName in files) {
-                var curFile = files[fileName];
-                if (curFile instanceof Object)
-                  return filter(curFile);
-                if (fileName.substr(fileName.length - 1) == 'a')
-                  delete files[fileName];
-              }
-            }
-            filter(srcFiles);
-            readDirFiles(out, 'utf8', function (outErr, outFiles) {
-              cb(outErr, srcFiles, outFiles);
-            });
-          });
-        },
-        'and destination files should match source files that pass filter': function (err, srcFiles, outFiles) {
-          assert.isNull(err);
-          assert.deepEqual(srcFiles, outFiles);
-        }
+      'and destination files should match source files that pass filter': function (err, srcFiles, outFiles) {
+        assert.isNull(err);
+        assert.deepEqual(srcFiles, outFiles);
       }
     }
   }
